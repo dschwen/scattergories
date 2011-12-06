55fd1f617f44c05413f2a546dbbd28cde37a924f
Daniel Schwen <daniel@schwen.de>
2011-12-06 10:44:46 -0600

    vote code

--- server.js
+++ server.js
@@ -10,15 +10,20 @@ var app = require('http').createServer(handler)
     ], cache = {}
   , alias = { '/': '/index.html' }
   , i,j,k
-  , cat = [ 'Rivers', 'Capitals', 'On the oceanside', 'Things you find in the forrest', 'Famous females', 'Superheroes', 
-            'Things you find at a carnival', 'Dangerous animals', 'Jewlery', 'Colors', 'Things you cannot afford',
-            'Things to bring on a camping trip', 'Every party needs this', 'In the cinema', 'At the mall', 'Famous monuments',
-            'Games', 'TV shows', 'Plants', 'Pets', 'At the cocktail bar', 'Funny movies', 'Scary things','Cars, makes & models',
-            'Bodies of water', 'Musical acts', 'Countries', 'Trouble with the law',  ]
+  , cat = [ 'Rivers', 'Capitals', 'On the oceanside', 'Things you find in the forrest', 
+            'Famous females', 'Superheroes', 'Things you find at a carnival', 
+            'Dangerous animals', 'Jewlery', 'Colors', 'Things you cannot afford',
+            'Things to bring on a camping trip', 'Every party needs this', 'In the cinema', 
+            'At the mall', 'Famous monuments', 'Games', 'TV shows', 'Plants', 'Pets', 
+            'At the cocktail bar', 'Funny movies', 'Scary things','Cars, makes & models',
+            'Bodies of water', 'Musical acts', 'Countries', 'Trouble with the law', 
+            'Car parts', 'Magazines and journals' ]
+          ]
   , selected = []
   , letters = 'ABCDEFGHIJKLMNOPRSTUVWZ'
   , roundletter
   , users = {}
+  , votes = {}
 
 // build file cache
 function addToCache(file) {
@@ -72,29 +77,55 @@ function handler( req, res ) {
   res.end(cache[req.url]);
 }
 
+
+// open vote and add callback
+function openVote(item,callback) {
+  votes[item] = callback;
+}
+// close vote
+function closeVote(item,callback) {
+  votes[item] = undefined;
+}
 // check for agreement on a consensus decission
 function checkConsensus(item) {
-  var current = undefined;
+  var current = undefined, nplayer = 0;
 
   for( id in users ) { 
     if( users.hasOwnProperty(id) ) {
-      if( !users[id].ready ) return;
+      nplayer++;
+      
+      // at least one player has not voted yet
+      if( users[id].vote[item] === undefined ) return undefined;
+      
+      if( current === undefined ) {
+        // looking at first vote
+        current = users[id].vote[item];
+      } else {
+        // are subsequent votes identical?
+        if( current !== users[id].vote[item] ) return undefined;
+      }
     }
   }
 
-  // reset ready states
+  // need more than one player for a consensus
+  if( nplayer <= 1 ) return undefined;
+  
+  // reset votes
   for( id in users ) { 
     if( users.hasOwnProperty(id) ) {
-      users[id].ready = false;
+      users[id].vote[item] = undefined;
     }
   }
 
+  // broadcast and return vote result
+  io.sockets.emit('vote', { item: item, outcome: current } );
+  return current;
 }
 
 io.sockets.on('connection', function (socket) {
 
   socket.emit( 'ready', { motd: '' } );
-  users[socket.id] = { ready: false, list: [], name: null };
+  users[socket.id] = { ready: false, list: [], name: null, vote: {} };
   
   socket.on('login', function (data) {
     users[socket.id].name = data.name;
@@ -106,24 +137,30 @@ io.sockets.on('connection', function (socket) {
     users[socket.id] = undefined;
   });
 
+  socket.on('vote', function (data) {
+    // is the vote open (callback set)
+    if( typeof(votes[data.item]) === 'function' ) {
+      users[socket.id].vote[data.item] = data.vote;
+      var consensus = checkConsensus(data.item);
+      if( consensus !== undefined ) {
+        (votes[data.item])(consensus);
+        closeVote(data.item);
+      }
+    } else {
+      console.log('Cheating! Voting on closed vote!');
+    };
+  });
 
   // user ready to start next letter
-  socket.on('ready', function (data) {
-    socket.broadcast.emit( 'ready', data );
-    users[socket.id].ready = true;
-    
-    // check if all users are ready
-
-    // emit contdown start signal
-    io.sockets.emit( 'startcountdown' );
-
+  openVote('ready',function() {
+    console.log('All players ready!')
     // deliver letter in 3 seconds
     setTimeout( function() {
       // pick letter
       roundletter = letters.substr(  Math.floor( Math.random() * letters.length ), 1 );
       io.sockets.emit( 'startgame', { letter: roundletter } );
     }, 3000);
-  } );
+  });
 
   // edit events get applied to the server copy of the map and rebroadcast to all clients
   socket.on('update', function (data) {
